617dc1b8fc895f23e31d65619abe12443645f82a
Jesus-Pons <jesponmor@alum.us.es>
2026-02-23 22:35:26 +0100

    feat: jugador 1 puntua funcionando

--- src/main/java/p3dev/ScoreTranslator.java
+++ src/main/java/p3dev/ScoreTranslator.java
@@ -3,6 +3,7 @@ package p3dev;
 public class ScoreTranslator {
 
     public String translate(int scorePlayerOne, int scorePlayerTwo){
+        if (scorePlayerOne == 1 && scorePlayerTwo == 0) return "fifteen-love";
         return "love-love";
     }
 }
