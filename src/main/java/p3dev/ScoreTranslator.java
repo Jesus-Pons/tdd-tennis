d431f713af004077fd8a1ff103d83468c870fd05
Jesus-Pons <jesponmor@alum.us.es>
2026-02-24 08:54:29 +0100

    feat: caso deuce funcionando

--- src/main/java/p3dev/ScoreTranslator.java
+++ src/main/java/p3dev/ScoreTranslator.java
@@ -5,6 +5,7 @@ public class ScoreTranslator {
     private final String[] scoreNames = {"love","fifteen","thirty","forty"};
 
     public String translate(int scorePlayerOne, int scorePlayerTwo){
+        if(scorePlayerOne == 3 && scorePlayerTwo== 3) return "deuce";
         if(scorePlayerOne==scorePlayerTwo && scorePlayerOne>0) return scoreNames[scorePlayerOne]+"-all";
         return scoreNames[scorePlayerOne] + "-" + scoreNames[scorePlayerTwo];
     }
