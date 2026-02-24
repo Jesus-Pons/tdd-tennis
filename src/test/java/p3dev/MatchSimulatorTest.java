4d7b955474787f1367144155a5e0461a67e0a1a4
Jesus-Pons <jesponmor@alum.us.es>
2026-02-24 15:19:20 +0100

    test: Test jugador 1 gana todo no compila

---
+++ src/test/java/p3dev/MatchSimulatorTest.java
@@ -0,0 +1,25 @@
+package p3dev;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MatchSimulatorTest {
+
+    @Test
+    void should_simulate_match_until_player_one_wins_four_games_to_zero() {
+        ScoreTranslator translator = new ScoreTranslator();
+
+        PointProvider mockProvider = mock(PointProvider.class);
+
+        when(mockProvider.getNextPointWinner()).thenReturn(1);
+
+        MatchSimulator simulator = new MatchSimulator(translator, mockProvider);
+
+        String matchResult = simulator.playMatch();
+
+        assertEquals("Match won by Player 1, Score: 4-0", matchResult);
+
+        verify(mockProvider, times(16)).getNextPointWinner();
+    }
+}
