3f8fd4ce06726d1cd24461de1e5ac8d2c93e6e02
Jesus-Pons <jesponmor@alum.us.es>
2026-02-24 15:59:30 +0100

    test: test gana jugador 2 simulación falla

--- src/test/java/p3dev/MatchSimulatorTest.java
+++ src/test/java/p3dev/MatchSimulatorTest.java
@@ -22,4 +22,19 @@ public class MatchSimulatorTest {
 
         verify(mockProvider, times(16)).getNextPointWinner();
     }
+    @Test
+    void should_simulate_match_until_player_two_wins_four_games_to_zero() {
+        ScoreTranslator translator = new ScoreTranslator();
+        PointProvider mockProvider = mock(PointProvider.class);
+
+        when(mockProvider.getNextPointWinner()).thenReturn(2);
+
+        MatchSimulator simulator = new MatchSimulator(translator, mockProvider);
+
+        String matchResult = simulator.playMatch();
+
+        assertEquals("Match won by Player 2, Score: 0-4", matchResult);
+
+        verify(mockProvider, times(16)).getNextPointWinner();
+    }
 }
