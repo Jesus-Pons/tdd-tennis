da00a93e0f0d631836c73f8e3efab517071510e8
Jesus-Pons <jesponmor@alum.us.es>
2026-02-24 08:17:50 +0100

    feat: Test jugador 1 puntua dos veces funcionando

--- src/main/java/p3dev/ScoreTranslator.java
+++ src/main/java/p3dev/ScoreTranslator.java
@@ -4,6 +4,7 @@ public class ScoreTranslator {
 
     public String translate(int scorePlayerOne, int scorePlayerTwo){
         if (scorePlayerOne == 1 && scorePlayerTwo == 0) return "fifteen-love";
+        else if(scorePlayerOne ==2 && scorePlayerTwo == 0) return "thirty-love";
         return "love-love";
     }
 }
