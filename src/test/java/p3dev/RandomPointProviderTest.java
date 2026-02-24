716fb85604e2ef8aa4cde4f5b3909b9f0c51176d
Jesus-Pons <jesponmor@alum.us.es>
2026-02-24 17:21:54 +0100

    test: Provider random devuelve valores correctos

---
+++ src/test/java/p3dev/RandomPointProviderTest.java
@@ -0,0 +1,20 @@
+package p3dev;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RandomPointProviderTest {
+
+    @Test
+    void should_always_return_one_or_two() {
+        RandomPointProvider provider = new RandomPointProvider();
+
+        for (int i = 0; i < 1000; i++) {
+            int result = provider.getNextPointWinner();
+
+            // Verificamos el contrato: Solo puede ser 1 o 2
+            assertTrue(result == 1 || result == 2,
+                    "El proveedor devolvió un valor inválido: " + result);
+        }
+    }
+}
